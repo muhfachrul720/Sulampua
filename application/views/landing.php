1ce1e67573fefc3ee3acc4900abe69191622f02d
Muh Fachrul <muhfachrul720@gmail.com>
2020-01-13 13:01:30 +0700

    Commit

---
+++ application/views/landing.php
@@ -0,0 +1,538 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Sulampua</title>
+	<link rel="stylesheet" href="<?= base_url()?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url()?>assets/custom/css/cs_landing_1.css">
+	<link rel="stylesheet" href="<?= base_url()?>assets/fontawesome/css/all.min.css">
+</head>
+<body>
+	<!-- Wrapper -->
+	<div class="wrapper">
+
+		<!-- Navbar -->
+		<div class="cs-navbar">
+			<div class="container">
+				<nav class="navbar navbar-expand-lg navbar-dark py-4">
+					<a href="" class="navbar-brand"><img src="<?= base_url()?>assets/img/kasulampua.png" alt="" width="140"></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav m-auto">
+							<!-- <li class="nav-item active">
+								<a class="nav-link mx-3 cs-btn" href="#">About <span class="sr-only">(current)</span></a>
+							</li> -->
+						</ul>
+						<form class="form-inline ml-auto">
+							<input class="mr-sm-2 cs-searchbox" type="search" placeholder="What Are You Looking For" aria-label="Search">
+						</form>
+						<button href="nav-link" id="searchButton" style="color:#ffffff; font-size:18px; border:none; background-color:transparent"><i class="fas fa-search"></i></button>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<!-- End of Navbar -->
+
+		<!-- Banner -->
+		<div class="banner">
+			<div class="bn-img-plc">
+				<div class="bn-img" style="background-image:url(<?= base_url()?>assets/img/banner.jpg)"></div>
+				<div class="bn-overlay">
+					<p> 
+						Kalimantan merupakan sebuah pulau di Indonesia yang belum banyak dijelajahi oleh wisatawan namun memiliki banyak keunikan dan daya tarik termasuk juga kaya dengan warisan budaya.
+					</p>
+					<span>Kalimantan</span>
+				</div>
+			</div>
+			<div class="bn-img-plc">
+				<div class="bn-img" style="background-image:url(<?= base_url()?>assets/img/banner_3.jpg)"></div>
+				<div class="bn-overlay">
+					<p> 
+					Nama Sulawesi diperkirakan berasal dari bahasa lokal, Sula yang artinya pulau dan Mesi yang berarti besi. Nama ini digunakan karena adanya perdagangan bijih besi, hasil produksi tambang-tambang di sekitar Danau Matano, 
+					</p>
+					<span>Sulawesi</span>
+				</div>
+			</div>
+			<div class="bn-img-plc">
+				<div class="bn-img" style="background-image:url(<?= base_url()?>assets/img/banner_4.jpg)"></div>
+				<div class="bn-overlay">
+					<p> 
+					Maluku adalah sebuah provinsi yang meliputi bagian selatan Kepulauan Maluku, Indonesia, Mesir menyebutkan adanya negeri dari timur yang sangat kaya, merupakan tanah surga, dengan hasil alam berupa cengkih, emas dan mutiara, daerah itu tak lain dan tak bukan adalah tanah Maluku
+					</p>
+					<span>Maluku</span>
+				</div>
+			</div>
+			<div class="bn-img-plc">
+				<div class="bn-img" style="background-image:url(<?= base_url()?>assets/img/banner.jpg)"></div>
+				<div class="bn-overlay">
+					<p>
+					Provinsi Papua memiliki luas sekitar 421.981 km2, pulau Papua berada di ujung timur dari wilayah Indonesia, dengan potensi sumber daya alam yang bernilai ekonomis dan strategis, dan telah mendorong bangsa-bangsa asing untuk menguasai pulau Papua. 
+					</p>
+					<span>Papua</span>
+				</div>
+			</div>
+		</div>
+		<!-- End of Banner -->
+
+		<!-- Data Terbaru -->
+		<section class="sect-data pt-5 pb-3">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 m-0">
+						<div class="col-lg-12">
+							<h2>Berita Terbaru</h2>
+						</div>
+						<div class="col-lg-12">
+							<div class="info-latest my-4">
+								<div class="row w-100">
+									<div class="col-lg-9">
+										<div class="title"> Sosial Dan Kesehatan </div>
+										<div class="summary">
+											Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iure recusandae nisi magnam ipsum quisquam quasi impedit pariatur doloremque perferendis, cumque molestiae mollitia atque nesciunt itaque sunt reprehenderit consequatur ullam?
+										</div>
+										<div class="date">12 Januari 2019</div>
+									</div>
+									<div class="col-lg-3 p-0 m-auto" style="text-align:center;">
+										<button class="btn btn-theme-1 btn-sm my-2 w-75">More..</button>
+										<button class="btn btn-theme-1 btn-sm my-2 w-75"><i class="fas fa-download"></i> Download</button>
+									</div>
+								</div>
+							</div>
+							<div class="info-latest my-4">
+								<div class="row w-100">
+									<div class="col-lg-9">
+										<div class="title"> Ekonomi Dan Perbankan </div>
+										<div class="summary">
+											Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iure recusandae nisi magnam ipsum quisquam quasi impedit pariatur doloremque perferendis, cumque molestiae mollitia atque nesciunt itaque sunt reprehenderit consequatur ullam?
+										</div>
+										<div class="date">12 Januari 2019</div>
+									</div>
+									<div class="col-lg-3 p-0 m-auto" style="text-align:center;">
+										<button class="btn btn-theme-1 btn-sm my-2 w-75">More..</button>
+										<button class="btn btn-theme-1 btn-sm my-2 w-75"><i class="fas fa-download"></i> Download</button>
+									</div>
+								</div>
+							</div>
+							<div class="info-latest my-4">
+								<div class="row w-100">
+									<div class="col-lg-9">
+										<div class="title"> Pertanian Dan Industri </div>
+										<div class="summary">
+											Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate iure recusandae nisi magnam ipsum quisquam quasi impedit pariatur doloremque perferendis, cumque molestiae mollitia atque nesciunt itaque sunt reprehenderit consequatur ullam?
+										</div>
+										<div class="date">12 Januari 2019</div>
+									</div>
+									<div class="col-lg-3 p-0 m-auto" style="text-align:center;">
+										<button class="btn btn-theme-1 btn-sm my-2 w-75">More..</button>
+										<button class="btn btn-theme-1 btn-sm my-2 w-75"><i class="fas fa-download"></i> Download</button>
+									</div>
+								</div>
+							</div>
+							<div class="info-latest my-4">
+								<div class="row w-100">
+									<div class="col-lg-12">
+										<a href=""><button class="btn btn-theme-1 btn-sm my-2 w-100">Selebihnya . . .</button></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 m-0 px-0">
+						<div class="col-sm-12 m-0 p-0">
+							<a href="">
+								<div class="data-box">
+								<i class="fas fa-angle-right cs-angle"></i>
+								<i class="fas fa-heartbeat"></i> <i class="fas fa-users"></i>
+								<p>Sosial dan Kesehatan</p>
+							</div>
+							</a>
+						</div>
+						<div class="col-sm-12 m-0 p-0">
+							<div class="data-box">
+								<i class="fas fa-angle-right cs-angle"></i>
+								<i class="fas fa-balance-scale"></i> <i class="fas fa-landmark"></i>
+								<p>Ekonomi Dan Bank</p>
+							</div>
+						</div>
+						<div class="col-sm-12 m-0 p-0">
+							<div class="data-box">
+								<i class="fas fa-angle-right cs-angle"></i>
+								<i class="fas fa-tractor"></i> <i class="fas fa-industry"></i>
+								<p>Pertanian Dan Industri</p>
+							</div>
+						</div>
+						<div class="col-sm-12 m-0 p-0">
+							<div class="data-box">
+								<i class="fas fa-angle-right cs-angle"></i>
+								<i class="fas fa-tasks"></i> 
+								<p>Perencanaan</p>
+							</div>
+						</div>
+						<div class="col-sm-12 m-0 p-0">
+							<div class="data-box">
+								<i class="fas fa-angle-right cs-angle"></i>
+								<i class="fas fa-archive"></i>
+								<p>Lainnya</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End of Data Terbaru -->
+
+		<!-- Graph -->
+		<div class="sect-about graph py-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 chart-info" style="text-align:left">
+						<h5>Grafik Data Terbaru</h5>
+						<h1>Tingkat Partisipasi Angkatan Kerja Tahun 2018</h1>
+						<div class="date">
+							<div class="info-bar"></div>
+							2019-06-14 00:03:12
+						</div>
+					</div>
+					<div class="col-lg-7 chart-bar">
+						<canvas id="myChart"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="sect-about graph py-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 chart-info" style="text-align:left">
+						<h5>Grafik Data Terbaru</h5>
+						<h1>Tingkat Partisipasi Angkatan Kerja Tahun 2018</h1>
+						<div class="date">
+							<div class="info-bar"></div>
+							2019-06-14 00:03:12
+						</div>
+					</div>
+					<div class="col-lg-7 chart-bar">
+						<canvas id="myChart2"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="sect-about graph py-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 chart-info" style="text-align:left">
+						<h5>Grafik Data Terbaru</h5>
+						<h1>Tingkat Partisipasi Angkatan Kerja Tahun 2018</h1>
+						<div class="date">
+							<div class="info-bar"></div>
+							2019-06-14 00:03:12
+						</div>
+					</div>
+					<div class="col-lg-7 chart-bar">
+						<canvas id="myChart3"></canvas>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End of Graph -->
+
+		<!-- About -->
+		<div class="sect-about about-us py-4">
+			<div class="img-container img-left">
+				<img src="<?= base_url()?>assets/img/Geo.jpg" alt="">
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 about-image">
+					</div>
+					<div class="col-lg-6 about-article themed my-4">
+						<div class="col-lg-12 p-0" id="geografis">
+							<div class="title themed mb-3"><h3>Letak Geografis</h3></div>
+							<p>Sulampua terdiri dari tiga pulau, yakni Pulau Sulawesi, Maluku dan Papua. Pulau Sulawesi terdiri dari enam provinsi, yakni Provinsi Sulawesi Utara, Sulawesi Tengah, Sulawesi Selatan, Sulawesi Tenggara, Sulawesi Barat dan Gorontalo. Sementara Kepulauan Maluku terdiri dari dua provinsi, yakni Provinsi Maluku dan Maluku Utara. Sedangkan Pulau Papua terdiri dari dua provinsi, yakni Provinsi Papua dan Papua Barat.</p> 
+						</div>
+						<div class="col-lg-12 p-0" id="about">
+							<div class="title themed mb-3"><h3>Keadaan Penduduk</h3></div>
+							<p>Sulampua terdiri dari tiga pulau, yakni Pulau Sulawesi, Maluku dan Papua. Pulau Sulawesi terdiri dari enam provinsi, yakni Provinsi Sulawesi Utara, Sulawesi Tengah, Sulawesi Selatan, Sulawesi Tenggara, Sulawesi Barat dan Gorontalo. Sementara Kepulauan Maluku terdiri dari dua provinsi, yakni Provinsi Maluku dan Maluku Utara. Sedangkan Pulau Papua terdiri dari dua provinsi, yakni Provinsi Papua dan Papua Barat.</p> 
+						</div>
+						<div class="col-lg-12 p-0" id="people">
+							<div class="title themed mb-3"><h3>Ketenagakerjaan</h3></div>
+							<p>Sulampua terdiri dari tiga pulau, yakni Pulau Sulawesi, Maluku dan Papua. Pulau Sulawesi terdiri dari enam provinsi, yakni Provinsi Sulawesi Utara, Sulawesi Tengah, Sulawesi Selatan, Sulawesi Tenggara, Sulawesi Barat dan Gorontalo. Sementara Kepulauan Maluku terdiri dari dua provinsi, yakni Provinsi Maluku dan Maluku Utara. Sedangkan Pulau Papua terdiri dari dua provinsi, yakni Provinsi Papua dan Papua Barat.</p> 
+						</div>
+						<div class="col-lg-12 p-0">
+							<button for="r1" class="about-checked" value="1"></button>
+							<button for="r2" class="about-checked" value="2"></button>
+							<button for="r3" class="about-checked" value="3"></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Geografi -->
+		<div class="footer py-4">
+			<div class="container">
+				<div class="row m-2">
+					<div class="col-lg-2">
+						Didukung Oleh : 
+					</div>
+					<div class="col-lg-3">
+						<h6 class="sm-title">Pemerintah Daerah Provinsi</h6>
+						<ul>
+							<li><a href="#">Sulawesi Utara</a></li>
+							<li><a href="#">Sulawesi Tengah</a></li>
+							<li><a href="#">Sulawesi Selatan</a></li>
+							<li><a href="#">Sulawesi Tenggara</a></li>
+							<li><a href="#">Gorontalo</a></li>
+							<li><a href="#">Sulawesi Barat</a></li>
+							<li><a href="#">Maluku</a></li>
+							<li><a href="#">Maluku Utara</a></li>
+							<li><a href="#">Papua Barat</a></li>
+							<li><a href="#">Papua</a></li>
+							<li><a href="#">Kalimantan</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3">
+						<h6 class="sm-title">Badan Pusat Statistik</h6>
+						<ul>
+							<li><a href="#">BPS Sulawesi Utara</a></li>
+							<li><a href="#">BPS Sulawesi Tengah</a></li>
+							<li><a href="#">BPS Sulawesi Selatan</a></li>
+							<li><a href="#">BPS Sulawesi Tenggara</a></li>
+							<li><a href="#">BPS Gorontalo</a></li>
+							<li><a href="#">BPS Sulawesi Barat</a></li>
+							<li><a href="#">BPS Maluku</a></li>
+							<li><a href="#">BPS Maluku Utara</a></li>
+							<li><a href="#">BPS Papua Barat</a></li>
+							<li><a href="#">BPS Papua</a></li>
+							<li><a href="#">BPS Kalimantan</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3">
+						<h6 class="sm-title">Bank Indonesia</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End of Geografi -->
+
+	</div>
+</body>
+<script src="<?= base_url()?>assets/js/jquery/jquery.min.js"></script>
+<script src="<?= base_url()?>assets/js/bootstrap.min.js"></script>
+<script src="<?= base_url()?>assets/js/chartjs/Chart.js"></script>
+<script>
+	// Init var
+	var trigger1 = 0;
+	var trigger2 = 0;
+
+	// Search Button
+	$('#searchButton').on('click', function(){
+		if(trigger1 == 0){
+			$('.cs-searchbox').css({'width':'380px', 'background-color':'rgba(255,255,255,0.4)'});
+			trigger1 = 1;
+		}
+		else {
+			$('.cs-searchbox').css({'width':'0px', 'background':'transparent'});
+			trigger1 = 0;
+		}
+	});
+
+	//Slide box
+	$('#slideBox').on('click', function(){
+		if(trigger2 == 0){
+			$('.slide-box').css('bottom','0');
+			trigger2 = 1;
+		}
+		else {
+			$('.slide-box').css('bottom','-60%');
+			trigger2 = 0;
+		}
+	});
+
+	// About us
+	$('.about-checked').on('click', function(){
+		var value = $(this).val()
+		// console.log(value);
+		if(value == 1){
+			$('#geografis').css('display', 'block');
+			$('#about').css('display', 'none');
+			$('#people').css('display', 'none');
+		}
+		else if (value == 2){
+			$('#geografis').css('display', 'none');
+			$('#about').css('display', 'block');
+			$('#people').css('display', 'none');
+		}
+		else {
+			$('#geografis').css('display', 'none');
+			$('#about').css('display', 'none');
+			$('#people').css('display', 'block');
+		}
+	});
+
+	// Chart 1 js
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type : 'bar',
+		data : {
+			labels : ["Kalimantan", "Sulawesi Utara", "Sulawesi Tengah", "Sulawesi Selatan", "Sulawesi Tenggara", "Gorontalo", "Sulawesi Barat", "Maluku", "Papua", "Papua Barat"],
+			datasets : [{
+				label : '# of Votes',
+				data : [12, 9, 3, 23 ,2 , 3, 2, 3, 4, 5, 6, 7,],
+				backgroundColor : [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor : [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth : 1
+			}]	
+		},
+		options : {
+			legend :{
+				display : false
+			},
+			scales: {
+				xAxes: [{
+					gridLines: {
+						display:false
+					},
+					stacked : false,
+					begitAtZero : true,
+					ticks : {
+						autoSkip:false,
+						maxRotation: 90,
+						minRotation: 90
+					}
+				}],
+				yAxes: [{
+					gridLines: {
+						display:false
+					}   
+				}]
+			}
+		}
+	});
+
+	var ctx = document.getElementById("myChart2").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type : 'bar',
+		data : {
+			labels : ["Kalimantan", "Sulawesi Utara", "Sulawesi Tengah", "Sulawesi Selatan", "Sulawesi Tenggara", "Gorontalo", "Sulawesi Barat", "Maluku", "Papua", "Papua Barat"],
+			datasets : [{
+				label : '# of Votes',
+				data : [12, 9, 3, 23 ,2 , 3, 2, 3, 4, 5, 6, 7,],
+				backgroundColor : [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor : [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth : 1
+			}]	
+		},
+		options : {
+			legend :{
+				display : false
+			},
+			scales: {
+				xAxes: [{
+					gridLines: {
+						display:false
+					},
+					stacked : false,
+					begitAtZero : true,
+					ticks : {
+						autoSkip:false,
+						maxRotation: 90,
+						minRotation: 90
+					}
+				}],
+				yAxes: [{
+					gridLines: {
+						display:false
+					}   
+				}]
+			}
+		}
+	});
+
+	var ctx = document.getElementById("myChart3").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type : 'bar',
+		data : {
+			labels : ["Kalimantan", "Sulawesi Utara", "Sulawesi Tengah", "Sulawesi Selatan", "Sulawesi Tenggara", "Gorontalo", "Sulawesi Barat", "Maluku", "Papua", "Papua Barat"],
+			datasets : [{
+				label : '# of Votes',
+				data : [12, 9, 3, 23 ,2 , 3, 2, 3, 4, 5, 6, 7,],
+				backgroundColor : [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor : [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth : 1
+			}]	
+		},
+		options : {
+			legend :{
+				display : false
+			},
+			scales: {
+				xAxes: [{
+					gridLines: {
+						display:false
+					},
+					stacked : false,
+					begitAtZero : true,
+					ticks : {
+						autoSkip:false,
+						maxRotation: 90,
+						minRotation: 90
+					}
+				}],
+				yAxes: [{
+					gridLines: {
+						display:false
+					}   
+				}]
+			}
+		}
+	});
+
+</script>
+</html>
+
